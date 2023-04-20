fb3d2612062d3f15b8798ae9017ca34f5ce8829d
kushalbakshi <kushalbakshi@gmail.com>
2023-04-20 17:08:48 -0500

    Initial tutorial notebook created

--- workflow_zstack/pipeline.py
+++ workflow_zstack/pipeline.py
@@ -1,8 +1,11 @@
-from element_animal import subject, surgery
-from element_calcium_imaging import imaging, imaging_report, scan
+import datajoint as dj
 from element_lab import lab
+from element_lab.lab import Lab, Location, Project, Protocol, Source, User
+from element_animal import subject, surgery
+from element_animal.subject import Subject
 from element_session import session_with_id as session
-from element_zstack import volume
+from element_calcium_imaging import imaging, imaging_report, scan
+from element_zstack import volume, volume_matching
 from element_zstack.export import bossdb
 
 from . import db_prefix
@@ -19,6 +22,7 @@ __all__ = [
     "subject",
     "surgery",
     "volume",
+    "volume_matching",
     "bossdb",
     "Device",
     "get_session_directory",
@@ -29,19 +33,19 @@ __all__ = [
 # ---------------------------------- Activate schemas ----------------------------------
 
 lab.activate(db_prefix + "lab")
-
-# subject.activate(db_prefix + "subject", linking_module=__name__)
-surgery.activate(db_prefix + "subject", db_prefix + "surgery", linking_module=__name__)
+subject.activate(db_prefix + "subject", linking_module=__name__)
+surgery.activate(db_prefix + "surgery", linking_module=__name__)
 
 Experimenter = lab.User
 session.activate(db_prefix + "session", linking_module=__name__)
 
 Equipment = Device
+Session = session.Session
+SessionDirectory = session.SessionDirectory
 imaging.activate(db_prefix + "imaging", db_prefix + "scan", linking_module=__name__)
 
 Mask = imaging.Segmentation.Mask
-Session = session.Session
-SessionDirectory = session.SessionDirectory
 Scan = scan.Scan
 volume.activate(db_prefix + "volume", linking_module=__name__)
+volume_matching.activate(db_prefix + "volume_matching")
 bossdb.activate(db_prefix + "bossdb", linking_module=__name__)
