ab844202f3bf3a565a57584cdb6a686a77e6fc0b
kushalbakshi <kushalbakshi@gmail.com>
2023-05-16 14:47:22 -0500

    Update `paths.py` to work with Codespaces

--- workflow_zstack/paths.py
+++ workflow_zstack/paths.py
@@ -6,33 +6,8 @@ from element_interface.utils import find_full_path
 from element_session import session_with_id as session
 
 
-def get_session_directory(session_key: dict) -> str:
-    """Return relative path from SessionDirectory table given key
-
-    Args:
-        session_key (dict): Key uniquely identifying a session
-
-    Returns:
-        path (str): Relative path of session directory
-    """
-    from .pipeline import session
-
-    # NOTE: fetch (vs. fetch1) permits dir to not exist, may be the case when saving
-    # slices directly from from BossDB into inferred dir based on BossDB structure
-    session_dir = (session.SessionDirectory & session_key).fetch("session_dir")
-
-    if len(session_dir) > 1:
-        raise ValueError(
-            f"Found >1 directory for this key:\n\t{session_key}\n\t{session_dir}"
-        )
-    elif len(session_dir) == 1:
-        return session_dir[0]
-    else:
-        return None
-
-
 def get_volume_root_data_dir() -> List[str]:
-    """Return root directory for ephys from 'vol_root_data_dir' in dj.config
+    """Return root directory for volumetric data in dj.config
 
     Returns:
         path (any): List of path(s) if available or None
@@ -43,7 +18,7 @@ def get_volume_root_data_dir() -> List[str]:
     elif not isinstance(vol_root_dirs, Sequence):
         return list(vol_root_dirs)
     else:
-        return pathlib.Path(vol_root_dirs[0])
+        return pathlib.Path(vol_root_dirs)
 
 
 def _find_files_by_type(scan_key, filetype: str):
@@ -68,7 +43,7 @@ def get_volume_tif_file(scan_key):
         FileNotFoundError: If the session directory or tiff files are not found.
     """
     # Folder structure: root / subject / session / .tif (raw)
-    sess_dir, tiff_filepaths = _find_files_by_type(scan_key, "*_stitched.tif")
+    sess_dir, tiff_filepaths = _find_files_by_type(scan_key, "*.tif")
     if tiff_filepaths:
         return tiff_filepaths
     else:
